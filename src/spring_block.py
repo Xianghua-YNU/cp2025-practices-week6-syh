884f1df23737f547e915358bba6ab0f891057932
SYH543 <1361920409@qq.com>
2025-04-02 11:16:50 +0800

    Update spring_block.py

--- src/spring_block.py
+++ src/spring_block.py
@@ -1,107 +1,118 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+import matplotlib.pyplot as plt
 
 
-def solve_ode_euler(step_num):
+def spring_block_ode(y, t, m, k):
     """
-    使用欧拉法求解弹簧 - 质点系统的常微分方程。
+    弹簧 - 物块系统的常微分方程。
 
     参数:
-    step_num (int): 模拟的步数
+    y (list): 包含位置和速度的列表 [x, v]
+    t (float): 时间
+    m (float): 物块质量
+    k (float): 弹簧系数
+
+    返回:
+    list: 包含位置和速度的导数的列表 [dx/dt, dv/dt]
+    """
+    x, v = y
+    dxdt = v
+    dvdt = -k * x / m
+    return [dxdt, dvdt]
+
+
+def solve_ode_odeint(m, k, t_end, num_points):
+    """
+    使用odeint求解弹簧 - 物块系统的常微分方程。
+
+    参数:
+    m (float): 物块质量
+    k (float): 弹簧系数
+    t_end (float): 模拟结束时间
+    num_points (int): 模拟的点数
 
     返回:
     tuple: 包含时间数组、位置数组和速度数组的元组
     """
-    # TODO: 创建存储位置和速度的数组
-    position = None
-    velocity = None
-
-    # TODO: 计算时间步长
-    time_step = None
-
-    # TODO: 设置初始位置和速度
-    
-    # TODO: 使用欧拉法迭代求解微分方程
-    
-    # TODO: 生成时间数组
-    time_points = None
-
-    return time_points, position, velocity
+    # 初始条件
+    y0 = [0, 1]
+    # 时间点
+    t = np.linspace(0, t_end, num_points)
+    # 使用odeint求解微分方程
+    sol = odeint(spring_block_ode, y0, t, args=(m, k))
+    position = sol[:, 0]
+    velocity = sol[:, 1]
+    return t, position, velocity
 
 
-def spring_mass_ode_func(state, time):
+def solve_ode_euler(m, k, step_num, t_end):
     """
-    定义弹簧 - 质点系统的常微分方程。
+    使用欧拉法求解弹簧 - 物块系统的常微分方程。
 
     参数:
-    state (list): 包含位置和速度的列表
-    time (float): 时间
-
-    返回:
-    list: 包含位置和速度的导数的列表
-    """
-    # TODO: 从状态中提取位置和速度
-    
-    # TODO: 计算位置和速度的导数
-    
-    return [0, 0]  # 替换为正确的返回值
-
-
-def solve_ode_odeint(step_num):
-    """
-    使用 odeint 求解弹簧 - 质点系统的常微分方程。
-
-    参数:
+    m (float): 物块质量
+    k (float): 弹簧系数
     step_num (int): 模拟的步数
+    t_end (float): 模拟结束时间
 
     返回:
     tuple: 包含时间数组、位置数组和速度数组的元组
     """
-    # TODO: 设置初始条件
-    initial_state = None
-    
-    # TODO: 创建时间点数组
-    time_points = None
-    
-    # TODO: 使用 odeint 求解微分方程
-    solution = None
-    
-    # TODO: 从解中提取位置和速度
-    position = None
-    velocity = None
-    
+    # 创建存储位置和速度的数组，长度为 step_num + 1
+    position = np.zeros(step_num + 1)
+    velocity = np.zeros(step_num + 1)
+
+    # 计算时间步长
+    time_step = t_end / step_num
+
+    # 设置初始位置和速度
+    position[0] = 0
+    velocity[0] = 1
+
+    # 使用欧拉法迭代求解微分方程
+    for i in range(step_num):
+        # 根据微分方程更新位置
+        position[i + 1] = position[i] + velocity[i] * time_step
+        # 根据微分方程更新速度
+        velocity[i + 1] = velocity[i] - (k / m) * position[i] * time_step
+
+    # 生成时间数组
+    time_points = np.arange(step_num + 1) * time_step
+
     return time_points, position, velocity
-
-
-def plot_ode_solutions(time_euler, position_euler, velocity_euler, time_odeint, position_odeint, velocity_odeint):
-    """
-    绘制欧拉法和 odeint 求解的位置和速度随时间变化的图像。
-
-    参数:
-    time_euler (np.ndarray): 欧拉法的时间数组
-    position_euler (np.ndarray): 欧拉法的位置数组
-    velocity_euler (np.ndarray): 欧拉法的速度数组
-    time_odeint (np.ndarray): odeint 的时间数组
-    position_odeint (np.ndarray): odeint 的位置数组
-    velocity_odeint (np.ndarray): odeint 的速度数组
-    """
-    # TODO: 创建图形并设置大小
-    
-    # TODO: 绘制位置对比图
-    
-    # TODO: 绘制速度对比图
-    
-    # TODO: 显示图形
-    pass
 
 
 if __name__ == "__main__":
-    # 模拟步数
-    step_count = 100
-    # TODO: 使用欧拉法求解
-    time_euler, position_euler, velocity_euler = solve_ode_euler(step_count)
-    # TODO: 使用 odeint 求解
-    time_odeint, position_odeint, velocity_odeint = solve_ode_odeint(step_count)
-    # TODO: 绘制对比结果
-    plot_ode_solutions(time_euler, position_euler, velocity_euler, time_odeint, position_odeint, velocity_odeint)
+    m = 1.0
+    k = 1.0
+    t_end = 2 * np.pi
+    num_points = 100
+
+    # 使用odeint求解
+    t_odeint, position_odeint, velocity_odeint = solve_ode_odeint(m, k, t_end, num_points)
+
+    # 使用欧拉法求解
+    t_euler, position_euler, velocity_euler = solve_ode_euler(m, k, num_points, t_end)
+
+    # 绘制位置随时间的变化
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.plot(t_odeint, position_odeint, label='odeint')
+    plt.plot(t_euler, position_euler, label='Euler')
+    plt.xlabel('Time')
+    plt.ylabel('Position')
+    plt.title('Position vs Time')
+    plt.legend()
+
+    # 绘制速度随时间的变化
+    plt.subplot(1, 2, 2)
+    plt.plot(t_odeint, velocity_odeint, label='odeint')
+    plt.plot(t_euler, velocity_euler, label='Euler')
+    plt.xlabel('Time')
+    plt.ylabel('Velocity')
+    plt.title('Velocity vs Time')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
